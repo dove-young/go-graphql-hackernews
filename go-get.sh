5f6a4de9ea3f7ea65d17e09c8d82318bef05f1e5
Bo Hua Yang <yangboh@cn.ibm.com>
2022-09-01 19:59:36 -0700

    create database

---
+++ go-get.sh
@@ -0,0 +1,8 @@
+go get github.com/99designs/gqlgen@v0.17.16
+go get github.com/99designs/gqlgen/codegen@v0.17.16
+go get github.com/99designs/gqlgen/codegen@v0.17.16
+go get github.com/99designs/gqlgen/internal/imports@v0.17.16
+go get github.com/99designs/gqlgen/codegen/config@v0.17.16
+go get github.com/99designs/gqlgen/internal/imports@v0.17.16
+
+go run github.com/99designs/gqlgen generate
